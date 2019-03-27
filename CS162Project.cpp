1ed59c353f211a7d4c249a678f25a909128501fe
G-God <38515198+G-God@users.noreply.github.com>
2019-03-27 21:10:29 +0800

    Add files via upload

---
+++ CS162Project.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+//schedule algorithm for first come, first serve
+int fcfs(int arr [])
+{
+	int index, arrival, burst, priority;
+	int counter = 0;
+	while(counter < burst)
+	{
+		
+	}
+	return 0;
+}
+
+int main()
+{
+    int numTestCase, numBlocks, numProcess;
+	string schedule;
+	
+	cin >> numTestCase;
+	cin >> numProcess >> schedule;
+	
+	int arrInt [3][numBlocks];
+	
+	/*
+	for(int i = 0; i < numTestCase; i++)
+	{
+		schedule.toUpperCase();
+		if(schedule == "FCFS")
+		{
+			for(int j = 0; j < numBlocks; j++)
+			{
+				//put fcfs algorithm codes here.
+			}
+		}
+	}
+	*/
+	cout << numTestCase << endl << numProcess << endl << schedule << endl;
+
+    return 0;
+}
