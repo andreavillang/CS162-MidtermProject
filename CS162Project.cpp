973b9f9fae26befe3b3c571ae7da61e6e6f1aad2
G-God <38515198+G-God@users.noreply.github.com>
2019-04-01 23:05:21 +0800

    This one has completed FCFS, SJF, and RR

--- CS162Project.cpp
+++ CS162Project.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -8,15 +9,16 @@ struct process{
 	int arrival;
 	int burst;
 	int priority;
+	bool scheduled;
 };
 
 //schedule algorithm for first come, first serve
 string fcfs(process procs[],int numProcess){
-	int i, j, k, min, temp, arrivalCounter;
+	int i, j, k, min, timeElapsed;
+	process temp;
 	string answer = "";
 	
-	//This one sorts the from least to greatest
-	//The least will be stored in min
+	//This one sorts the from smalles to biggest arrival time
 	for(i = 0; i < numProcess - 1; i++){
 		min = i;
 		for(j = i + 1; j < numProcess; j++){
@@ -28,88 +30,230 @@ string fcfs(process procs[],int numProcess){
 				if(procs[j].index < procs[min].index){
 					min = j;
 				}
-				else{
-					min = min;
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Since first proc[0].arrival is the smallest arrival time, initialize it to timeElapsed
+	timeElapsed = procs[0].arrival;
+	
+	//Actual Algorithm
+	for(k = 0; k < numProcess; k++){
+		//Print answer first then add the burst to the timeElapsed
+		answer = answer + to_string(timeElapsed) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
+		timeElapsed += procs[k].burst;
+		
+		//Makes the timeElapsed dynamically changing
+		if(timeElapsed < procs[k+1].arrival){
+			timeElapsed = procs[k+1].arrival;
+		}
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//Schedule algorithm for Shortest Job First
+string sjf(process procs[],int numProcess){
+	int i, j, k, min, timeElapsed = 0, done = 0;
+	process temp;
+	string answer = "";
+	
+	//This one sorts the from smallest to biggest burst time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].burst < procs[min].burst){
+				min = j;
+			}
+			//This takes into account when there are similar burst, choose smallest index
+			else if(procs[j].burst == procs[min].burst){
+				if(procs[j].index < procs[min].index){
+					min = j;
 				}
 			}
 		}
-		//Sorting Arrival
-		temp = procs[i].arrival;
-		procs[i].arrival = procs[min].arrival;
-		procs[min].arrival = temp;
-		
-		//since first proc[i].arrival is the smallest arrival time, initialize it to arrivalCounter
-		arrivalCounter = procs[i].arrival;
-		
-		//Sorting Burst
-		temp = procs[i].burst;
-		procs[i].burst = procs[min].burst;
-		procs[min].burst = temp;
-		
-		//Sorting Index
-		temp = procs[i].index;
-		procs[i].index = procs[min].index;
-		procs[min].index = temp;
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
 	}
-	//prints answers
+	
+	//Actual Algorithm
+	while(done != numProcess){
+		for(int k = 0; k < numProcess; k++){
+			//Check if arrival time is less than or equal to timeElapsed while also checking if already scheduled
+			if(procs[k].arrival <= timeElapsed && procs[k].scheduled == false){
+				answer = answer + to_string(timeElapsed) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
+				timeElapsed += procs[k].burst;
+				procs[k].scheduled = true;
+				done++;
+				timeElapsed--;
+				break;
+			}
+		}
+		//After every iteration increment timeElapsed
+		timeElapsed++;
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//Schedule algorithm for Round Robin
+string rr(process procs[], int numProcess, int quantumTime){
+	int i, j, k, min, timeElapsed, done = 0;
+	string answer = "";
+	process temp;
+	queue <process> q;
+	queue <process> qTemp;
+	
+	//This one sorts the from smallest to biggest arrival time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].arrival < procs[min].arrival){
+				min = j;
+			}
+			//This takes into account when there are similar arrival, choose smallest index
+			else if(procs[j].arrival == procs[min].arrival){
+				if(procs[j].index < procs[min].index){
+					min = j;
+				}
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Pushes all the elements into a queue
 	for(k = 0; k < numProcess; k++){
-		answer = answer + to_string(arrivalCounter) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
-		
-		//makes the time elapsed dynamically changing
-		if(procs[k].arrival < procs[k+1].arrival){
-			arrivalCounter = procs[k+1].arrival;
+		q.push(procs[k]);
+	}
+	
+	//Since first proc[0].arrival is the smallest arrival time, initialize it to timeElapsed
+	timeElapsed = procs[0].arrival;
+	
+	//Actual algorithm
+	while(done != numProcess){
+		//If the temporary queue is empty, no interrupt, then keep doing the original queue
+		if(qTemp.empty()){
+			//Traverses the regular queue
+			if(q.front().arrival <= timeElapsed){
+				//If less than quantumTime then finish it, update timeElapsed, then pop
+				if(q.front().burst < quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(q.front().burst) + "X" + "\n";
+					timeElapsed += q.front().burst;
+					q.pop();
+					done++;
+				}
+				//Else update timeElapsed, push into qTemp, then pop
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					q.front().burst -= quantumTime;
+					qTemp.push(q.front());
+					q.pop();
+				}
+			}
+			//Increment timeElapsed so that it doesn't become an infinite loop
+			else{
+				timeElapsed++;
+			}
 		}
-		else if(procs[k].arrival == procs[k+1].arrival){
-			arrivalCounter += procs[k].arrival;
+		//If temporary queue is not empty, there's an interrupt, then check both the original and temporary queue to see which one goes first
+		else if(!qTemp.empty()){
+			//Checks to see if original queue will run
+			if(q.front().arrival <= timeElapsed){
+				//Same one as above
+				if(q.front().burst < quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(q.front().burst) + "X" + "\n";
+					timeElapsed += q.front().burst;
+					q.pop();
+					done++;
+				}
+				//Same one as above
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					q.front().burst -= quantumTime;
+					qTemp.push(q.front());
+					q.pop();
+				}
+			}
+			//Else continue the interrupted process
+			else{
+				//Same one as above only this time with the qTemp
+				if(qTemp.front().burst < quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(qTemp.front().index) + " " + to_string(qTemp.front().burst) + "X" + "\n";
+					timeElapsed += qTemp.front().burst;
+					qTemp.pop();
+					done++;
+				}
+				//Same one as above only this time with the qTemp
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(qTemp.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					qTemp.front().burst -= quantumTime;
+					qTemp.push(qTemp.front());
+					qTemp.pop();
+				}
+			}
 		}
 	}
+	
+	//Print Answer
 	return answer;
 }
 
 int main(){
-    int numTestCase, numProcess, a, b, p;
+    int numTestCase, numProcess;
 	int quantumTime;
 	string schedule;
 	
 	cin >> numTestCase;
-	cin >> numProcess >> schedule;
-	
-	process proc[numProcess];
 	
 	for(int i = 1; i <= numTestCase; i++){
+		cin >> numProcess >> schedule;
+		process proc[numProcess];
 		transform(schedule.begin(), schedule.end(), schedule.begin(), ::toupper);
+		
 		if(schedule == "FCFS"){
 			for(int index = 1; index <= numProcess; index++){
 				proc[index - 1].index = index;
-				cin >> a >> b >> p;
-				proc[index - 1].arrival = a;
-				proc[index - 1].burst = b;
-				proc[index - 1].priority = p;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
 			}
-			cout << fcfs(proc,numProcess);
+			cout << i << endl << fcfs(proc,numProcess) << endl;
 		}
 		else if(schedule == "SJF"){
 			for(int index = 1; index <= numProcess; index++){
-				//put sjf algorithm codes here.
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+				proc[index-1].scheduled = false;
 			}
+			cout << i << endl << sjf(proc,numProcess) << endl;
 		}
 		else if(schedule == "SRTF"){
 			for(int index = 1; index <= numProcess; index++){
-				//put srtf algorithm codes here.
+				//put SRTF algorithm codes here.
 			}
 		}
 		else if(schedule == "P"){
 			for(int index = 1; index <= numProcess; index++){
-				//put p algorithm codes here.
+				//put P algorithm codes here.
 			}
 		}
 		else if(schedule == "RR"){
 			cin >> quantumTime;
 			for(int index = 1; index <= numProcess; index++){
-				//put rr algorithm codes here.
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
 			}
+			cout << i << endl << rr(proc,numProcess,quantumTime) << endl;
 		}
 	}
-
     return 0;
 }
