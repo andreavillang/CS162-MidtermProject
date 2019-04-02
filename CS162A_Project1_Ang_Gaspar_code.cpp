3239464c568e9f2f8ad6d890f547f953ff8deebe
Andrea Ang <andreavillang@gmail.com>
2019-04-02 22:08:12 +0800

    files added

---
+++ CS162A_Project1_Ang_Gaspar_code.cpp
@@ -0,0 +1,487 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct process{
+	int index;
+	int arrival;
+	int burst;
+	int priority;
+	bool scheduled = false;
+};
+
+//gets the smallest arrival from the processes and returns its index
+int getSmallestArrival(process procs[],int timeElapsed,int numProcess){
+    int min;
+    //checks if a process has not been scheduled to start with that as the min
+    for(int i = 0; i < numProcess; i++){
+        if(procs[i].scheduled == false){
+            min = i;
+            break;
+        }
+    }
+    for(int i = 0; i < numProcess; i++){
+        if(procs[i].scheduled == false){
+        	//this one gets the min immediately if it has an arrival less than or equal to the timeElapsed
+            if(procs[i].arrival <= timeElapsed){
+                min = i;
+                break;
+            }
+            //this one compares arrival times to the current min
+            if(procs[i].arrival < procs[min].arrival){
+                min = i;
+            }
+        }
+    }
+    return min;
+}
+
+//gets the next smallest arrival that might
+//...interrupt the current process and returns its index
+int getNextArrival(process procs[],int bound){
+	//bound is everything before the current running process
+    int min;
+    //checks if a process has not been scheduled to start with that as the min
+    for(int i = 0; i < bound; i++){
+        if(procs[i].scheduled == false){
+            min = i;
+            break;
+        }
+    }
+    for(int i = 0; i < bound; i++){
+        if(procs[i].scheduled == false && procs[i].arrival < procs[min].arrival){
+        	//arrival of procs[i] must be smaller than arrival of current process
+            min = i;
+        }
+    }
+    return min;
+}
+
+
+//schedule algorithm for first come, first serve
+string fcfs(process procs[],int numProcess){
+	int i, j, k, min, timeElapsed;
+	process temp;
+	string answer = "";
+	
+	//This one sorts the from smallest to biggest arrival time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].arrival < procs[min].arrival){
+				min = j;
+			}
+			//This takes into account when there are similar arrival, choose smallest index
+			else if(procs[j].arrival == procs[min].arrival){
+				if(procs[j].index < procs[min].index){
+					min = j;
+				}
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Since first proc[0].arrival is the smallest arrival time, initialize it to timeElapsed
+	timeElapsed = procs[0].arrival;
+	
+	//Actual Algorithm
+	for(k = 0; k < numProcess; k++){
+		//Print answer first then add the burst to the timeElapsed
+		answer = answer + to_string(timeElapsed) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
+		timeElapsed += procs[k].burst;
+		
+		//Makes the timeElapsed dynamically changing
+		if(timeElapsed < procs[k+1].arrival){
+			timeElapsed = procs[k+1].arrival;
+		}
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//Schedule algorithm for Shortest Job First
+string sjf(process procs[],int numProcess){
+	int i, j, k, min, timeElapsed = 0, done = 0;
+	process temp;
+	string answer = "";
+	
+	//This one sorts the from smallest to biggest burst time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].burst < procs[min].burst){
+				min = j;
+			}
+			//This takes into account when there are similar burst, choose smallest index
+			else if(procs[j].burst == procs[min].burst){
+				if(procs[j].index < procs[min].index){
+					min = j;
+				}
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Actual Algorithm
+	while(done != numProcess){
+		for(int k = 0; k < numProcess; k++){
+			//Check if arrival time is less than or equal to timeElapsed while also checking if already scheduled
+			if(procs[k].arrival <= timeElapsed && procs[k].scheduled == false){
+				answer = answer + to_string(timeElapsed) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
+				timeElapsed += procs[k].burst;
+				procs[k].scheduled = true;
+				done++;
+				timeElapsed--;
+				break;
+			}
+		}
+		//After every iteration increment timeElapsed
+		timeElapsed++;
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//Schedule algorithm for Round Robin
+string rr(process procs[], int numProcess, int quantumTime){
+	int i, j, k, min, timeElapsed, done = 0;
+	string answer = "";
+	process temp;
+	queue <process> q;
+	queue <process> qTemp;
+	
+	//This one sorts the from smallest to biggest arrival time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].arrival < procs[min].arrival){
+				min = j;
+			}
+			//This takes into account when there are similar arrival, choose smallest index
+			else if(procs[j].arrival == procs[min].arrival){
+				if(procs[j].index < procs[min].index){
+					min = j;
+				}
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Pushes all the elements into a queue
+	for(k = 0; k < numProcess; k++){
+		q.push(procs[k]);
+	}
+	
+	//Since first proc[0].arrival is the smallest arrival time, initialize it to timeElapsed
+	timeElapsed = procs[0].arrival;
+	
+	//Actual algorithm
+	while(done != numProcess){
+		//If the temporary queue is empty, no interrupt, then keep doing the original queue
+		if(qTemp.empty()){
+			//Traverses the regular queue
+			if(q.front().arrival <= timeElapsed && !q.empty()){
+				//If less than quantumTime then finish it, update timeElapsed, then pop
+				if(q.front().burst <= quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(q.front().burst) + "X" + "\n";
+					timeElapsed += q.front().burst;
+					q.pop();
+					done++;
+				}
+				//Else update timeElapsed, push into qTemp, then pop
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					q.front().burst -= quantumTime;
+					qTemp.push(q.front());
+					q.pop();
+				}
+			}
+			//Increment timeElapsed so that it doesn't become an infinite loop
+			else{
+				timeElapsed++;
+			}
+		}
+		//If temporary queue is not empty, there's an interrupt, then check both the original and temporary queue to see which one goes first
+		else if(!qTemp.empty()){
+			//Checks to see if original queue will run
+			if(q.front().arrival <= timeElapsed && !q.empty()){
+				//Same one as above
+				if(q.front().burst <= quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(q.front().burst) + "X" + "\n";
+					timeElapsed += q.front().burst;
+					q.pop();
+					done++;
+				}
+				//Same one as above
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(q.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					q.front().burst -= quantumTime;
+					qTemp.push(q.front());
+					q.pop();
+				}
+			}
+			//Else continue the interrupted process
+			else{
+				//Same one as above only this time with the qTemp
+				if(qTemp.front().burst <= quantumTime){
+					answer = answer + to_string(timeElapsed) + " " + to_string(qTemp.front().index) + " " + to_string(qTemp.front().burst) + "X" + "\n";
+					timeElapsed += qTemp.front().burst;
+					qTemp.pop();
+					done++;
+				}
+				//Same one as above only this time with the qTemp
+				else{
+					answer = answer + to_string(timeElapsed) + " " + to_string(qTemp.front().index) + " " + to_string(quantumTime) + "\n";
+					timeElapsed += quantumTime;
+					qTemp.front().burst -= quantumTime;
+					qTemp.push(qTemp.front());
+					qTemp.pop();
+				}
+			}
+		}
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//schedule algorithm for priority preemptive
+string priorityP(process procs[],int numProcess){
+	int timeElapsed = 0;
+	bool allDone = false;
+	bool allDoneChecker = false;
+	process procsTemp;
+	string answer = "";
+
+	//sorts the array of processes by shortest priority times
+	for(int i = 0; i < numProcess-1; i++){
+		for(int j = 0; j < numProcess-i-1; j++){
+			if(procs[j].priority > procs[j+1].priority) {
+				procsTemp = procs[j];
+				procs[j] = procs[j+1];
+				procs[j+1] = procsTemp;
+			}
+			else if(procs[j].priority == procs[j+1].priority){
+				if(procs[j].index > procs[j+1].index){
+					procsTemp = procs[j];
+					procs[j] = procs[j+1];
+					procs[j+1] = procsTemp;
+				}
+			}
+		}
+	}
+    
+    //while not all the processes are done, loop
+    while(!allDone){
+    	//first gets the starting index of the process with the smallest arrival
+        int startIndex = getSmallestArrival(procs, timeElapsed, numProcess);
+        //then gets the index of the next process with the smallest arrival
+        //...that might interrupt the current process
+        int nextIndex = getNextArrival(procs, startIndex);
+        //set timeElapsed to the current running time
+        if(timeElapsed < procs[startIndex].arrival){
+            timeElapsed = procs[startIndex].arrival + 1;
+        }
+        //this will run if a process is interrupted
+        if(startIndex != nextIndex && procs[startIndex].burst+timeElapsed - 1 > procs[nextIndex].arrival){
+            answer += to_string(timeElapsed) + " " + to_string(procs[startIndex].index) + " " + to_string(procs[nextIndex].arrival - procs[startIndex].arrival)  + "\n";
+            
+			timeElapsed = procs[nextIndex].arrival;
+            procs[startIndex].burst -= (timeElapsed-procs[startIndex].arrival);
+            procs[startIndex].arrival = procs[nextIndex].arrival + procs[nextIndex].burst;
+        }
+        //this will run if a process will complete
+        else{
+            answer += to_string(timeElapsed) + " " + to_string(procs[startIndex].index) + " " + to_string(procs[startIndex].burst) + "X" + "\n";
+            timeElapsed += procs[startIndex].burst;
+            procs[startIndex].scheduled = true;
+        }
+
+        //checks if all processes have been scheduled which means they are all done
+        allDoneChecker = true;
+		for(int i = 0; i < numProcess; i++){
+			if(procs[i].scheduled == false){
+				allDoneChecker = false;
+				break;
+			}
+		}
+		allDone = allDoneChecker;
+    }
+    return answer;
+}
+
+//schedule algorithm for priority non-preemptive
+string priorityNP(process procs[],int numProcess){
+	int i, j, k, min, timeElapsed = 0, done = 0;
+	process temp;
+	string answer = "";
+	
+	//This one sorts the from smallest to biggest priority time
+	for(i = 0; i < numProcess - 1; i++){
+		min = i;
+		for(j = i + 1; j < numProcess; j++){
+			if(procs[j].priority < procs[min].priority){
+				min = j;
+			}
+			//This takes into account when there are similar priority, choose smallest index
+			else if(procs[j].priority == procs[min].priority){
+				if(procs[j].index < procs[min].index){
+					min = j;
+				}
+			}
+		}
+		temp = procs[i];
+		procs[i] = procs[min];
+		procs[min] = temp;
+	}
+	
+	//Actual Algorithm
+	while(done != numProcess){
+		for(int k = 0; k < numProcess; k++){
+			//Check if arrival time is less than or equal to timeElapsed while also checking if already scheduled
+			if(procs[k].arrival <= timeElapsed && procs[k].scheduled == false){
+				answer = answer + to_string(timeElapsed) + " " + to_string(procs[k].index) + " " + to_string(procs[k].burst) + "X" + "\n";
+				timeElapsed += procs[k].burst;
+				procs[k].scheduled = true;
+				done++;
+				timeElapsed--;
+				break;
+			}
+		}
+		//After every iteration increment timeElapsed
+		timeElapsed++;
+	}
+	
+	//Print Answer
+	return answer;
+}
+
+//schedule algorithm for shortest job first preemptive
+string srtf(process procs[],int numProcess){
+	int timeElapsed = 0;
+	bool allDone = false;
+	bool allDoneChecker = false;
+	process procsTemp;
+	string answer = "";
+
+	//sorts the array of processes by shortest burst times
+	for(int i = 0; i < numProcess-1; i++){
+		for(int j = 0; j < numProcess-i-1; j++){
+			if(procs[j].burst > procs[j+1].burst) {
+				procsTemp = procs[j];
+				procs[j] = procs[j+1];
+				procs[j+1] = procsTemp;
+			}
+		}
+	}
+    
+    //while not all the processes are done, loop
+    while(!allDone){
+    	//first gets the starting index of the process with the smallest arrival
+        int startIndex = getSmallestArrival(procs, timeElapsed, numProcess);
+        //then gets the index of the next process with the smallest arrival
+        //...that might interrupt the current process
+        int nextIndex = getNextArrival(procs, startIndex);
+        //set timeElapsed to the current running time
+        if(timeElapsed < procs[startIndex].arrival){
+            timeElapsed = procs[startIndex].arrival + 1;
+        }
+        //this will run if a process is interrupted
+        if(startIndex != nextIndex && procs[startIndex].burst+timeElapsed - 1 > procs[nextIndex].arrival){
+            answer += to_string(timeElapsed) + " " + to_string(procs[startIndex].index) + " " + to_string(procs[nextIndex].arrival - procs[startIndex].arrival)  + "\n";
+            
+			timeElapsed = procs[nextIndex].arrival;
+            procs[startIndex].burst -= (timeElapsed-procs[startIndex].arrival);
+            procs[startIndex].arrival = procs[nextIndex].arrival + procs[nextIndex].burst;
+        }
+        //this will run if a process will complete
+        else{
+            answer += to_string(timeElapsed) + " " + to_string(procs[startIndex].index) + " " + to_string(procs[startIndex].burst) + "X" + "\n";
+            timeElapsed += procs[startIndex].burst;
+            procs[startIndex].scheduled = true;
+        }
+
+        //checks if all processes have been scheduled which means they are all done
+        allDoneChecker = true;
+		for(int i = 0; i < numProcess; i++){
+			if(procs[i].scheduled == false){
+				allDoneChecker = false;
+				break;
+			}
+		}
+		allDone = allDoneChecker;
+    }
+    return answer;
+}
+
+int main(){
+    int numTestCase, numProcess;
+	int quantumTime;
+	string schedule;
+	
+	cin >> numTestCase;
+	
+	for(int i = 1; i <= numTestCase; i++){
+		cin >> numProcess >> schedule;
+		process proc[numProcess];
+		transform(schedule.begin(), schedule.end(), schedule.begin(), ::toupper);
+		
+		if(schedule == "FCFS"){
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+			}
+			cout << i << endl << fcfs(proc,numProcess) << endl;
+		}
+		else if(schedule == "SJF"){
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+				proc[index-1].scheduled = false;
+			}
+			cout << i << endl << sjf(proc,numProcess) << endl;
+		}
+		else if(schedule == "SRTF"){
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+			}
+			cout << i << endl << srtf(proc,numProcess) << endl;
+		}
+		else if(schedule == "P"){
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+			}
+			cout << i << endl << priorityP(proc,numProcess) << endl;
+		}
+		else if(schedule == "PNP"){
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+			}
+			cout << i << endl << priorityNP(proc,numProcess) << endl;
+		}
+		else if(schedule == "RR"){
+			cin >> quantumTime;
+			for(int index = 1; index <= numProcess; index++){
+				proc[index - 1].index = index;
+				cin >> proc[index - 1].arrival >> proc[index - 1].burst >> proc[index - 1].priority;
+			}
+			cout << i << endl << rr(proc,numProcess,quantumTime) << endl;
+		}
+	}
+    return 0;
+}
---
+++ CS162A_Project1_Ang_Gaspar_readme.txt
@@ -0,0 +1,123 @@
+CS162 - Process Scheduling
+160096 Andrea Ang
+155498 Genesis Gaspar
+
+
+I. Description
+This project replicates different types of CPU scheduling functions such as:
+* First Come First Serve (FCFS)
+* Shortest Job First (SJF)
+* Shortest Remaining Time First (SRTF)
+* Round Robin (RR)
+* Priority Preemptive (P)
+* Priority Non-Preemptive (PNP)
+The Input starts with the number of test cases, which is then followed by blocks of n test cases. The output prints out the blocks for each test case starting with the test case number.
+
+
+Example Input:
+2
+4 FCFS
+0 50 2
+40 2 3
+20 3 1
+30 55 1
+4 SRTF
+0 50 2
+40 2 3
+20 3 1
+30 55 1
+
+Example Output:
+1
+0 1 50X
+50 3 3X
+53 4 55X
+108 2 2X
+2
+0 1 20
+20 3 3X
+23 1 17
+40 2 2X
+42 1 13X
+55 4 55X
+
+
+struct process{
+        int index;
+        int arrival;
+        int burst;
+        int priority;
+        bool scheduled = false;
+};
+
+
+This struct is used to indicate the, index, arrival, burst and priority of each process. In addition, a boolean called scheduled is added to indicated whether or not a process has completely ran or not.
+
+
+II. Methods
+
+
+1. getSmallestArrival(process procs[], int timeElapsed, int numProcess)
+   * This method will take in the process array, time that has passed, and the number of processes.
+   * It first checks if the process has not been scheduled to start yet.
+   * After that it checks for the smallest arrival time whilst taking into consideration the processes that have already been ran.
+   * It then returns the index of the process that hasn’t run yet and has the smallest arrival time.
+
+
+2. getNextArrival(process procs[], int bound)
+   * This method will take in the process array and the bound, which is the index returned by getSmallestArrival.
+   * It gets the next process with the smallest arrival time that might interrupt the current running process fetched by the previous method.
+   * If it returns the same index as the previous method, the process will not be interrupted.
+
+
+3. fcfs(process procs[], int numProcess)
+   * This method will take in the process array and the number of individual processes.
+   * Processes are sorted by smallest arrival time then index if the arrival times are the same.
+   * timeElapsed starts at the first process’ arrival time.
+   * Processes are then printed out in order.
+
+
+4. sjf(process procs[], int numProcess)
+   * This method takes in the process array and the number of processes.
+   * It will first sort the process array from smallest to biggest burst time.
+   * If there are similar burst times then it will be sorted by smallest index.
+   * After that it will run in a while loop that first checks if the process has been scheduled and then checks if the arrival time of that process is less than or equal to the time that has elapsed so far.
+   * If it hits it will print out the answer, update the timeElapsed, update the scheduling to true, update the number of processes done, decrement the timeElapsed, and then will break from there.
+   * After every iteration or when the iteration breaks, it will increment the timeElapsed so as not to cause an infinite loop.
+   * Returns the answer after running the while loop.
+
+5. srtf(process procs[], int numProcess)
+   * This method will take in the process array and the number of individual processes.
+   * Processes are sorted by smallest burst time.
+   * A boolean called allDone is set to false and will cause a loop to keep going as long as not all processes have been scheduled
+   * startIndex uses the method getSmallestArrival to get the process (that hasn’t been scheduled) with the smallest arrival time.
+   * nextIndex uses the method getNextArrival to get the process (that hasn’t been scheduled) with the smallest arrival time that will most likely interrupt the startIndex process.
+   * If nextIndex and startIndex are the same, the current process will not be interrupted.
+   * timeElapsed is set to the arrival time of the current running process + 1.
+   * If a process is interrupted, timeElapsed, burst and arrival are all updated accordingly.
+   * If a process is not interrupted, timeElapsed is updated and its scheduled boolean will be set to true to avoid repitition.
+   * A smaller loop within the while loop will check if any single process has not been scheduled yet to continue the algorithm.
+
+
+6. p(process procs[], int numProcess)
+   * The method runs the same as srtf except the processes are sorted by smallest priority then index if priorities are the same.
+
+
+7. pnp(process procs[], int numProcess)
+   * The method runs the same as sjf except the processes are sorted by smallest priority then index if the priorities are the same.
+
+
+8. rr(process procs[], int numProcess, int quantumTime)
+   * This method will take in the process array, number of processes and the quantum time.
+   * It will then sort the process array from smallest to biggest arrival time.
+   * Then it will push all the elements of the array into a queue.
+   * The time elapsed is set to the arrival time of the first processes so as not to delay things.
+   * It will be ran into a while loop where first it will check if another queue called qTemp is empty or not.
+   * If qTemp is empty then there are no interrupted processes so we continue on by checking if arrival time of the first element in the original queue called q will have less than or equal to the timeElapsed.
+   * If it hits then it prints the answer, updates the timeElapsed by adding its burst time, pops it from the queue since it is no longer needed, then finally updates that a process has been completed.
+   * Otherwise, prints the answer, updates timeElapsed by adding the quantum time instead, updates the burst time of that element by subtracting it from the quantum time, pushes it into qTemp, then pops it from q.
+   * Finally it increments timeElapsed if  the arrival time of the original queue is not less than or equal to the timeElapsed.
+   * On the other hand if qTemp is not empty then it first checks the original queue if something can be ran from there by still doing the same check from above.
+   * If it hits then it will also do the same methods as previously stated.
+   * Otherwise it will do the processes instead from qTemp and does the same job again this time it will do things from the qTemp instead of q.
+   * Returns the answer after running the while loop.
